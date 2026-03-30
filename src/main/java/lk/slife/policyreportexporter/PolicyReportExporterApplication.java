53e741e2f6a5fc088a8ea2041d30e2e43704ede1
Janith Payagala <Janith.Thiwanka@softlogiclife.lk>
2026-03-30 11:39:04 +0530

    initial commit

---
+++ src/main/java/lk/slife/policyreportexporter/PolicyReportExporterApplication.java
@@ -0,0 +1,13 @@
+package lk.slife.policyreportexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyReportExporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolicyReportExporterApplication.class, args);
+    }
+
+}
